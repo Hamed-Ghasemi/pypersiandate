55668399dd2d6cf1e0e8019f3a6f5343469af17e
Hamed-Ghasemi <hamed.ghasemi.gl@gmail.com>
2020-12-13 14:21:20 +0330

    update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pypersiandate
- Python library for converting gregorian date to persian date.
+
+Python library for converting gregorian date to persian date.
+
+```python
+    res = topersia(2020,1,1)
+print(res['day'])
+print(res['dayname'])
+print(res['year'])
+print(res['month'])
+print(res['monthname'])
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pypersiandate',
+    version='0.0.1',
+    description='Python library for converting gregorian date to persian date.',
+    author='Hamed Ghasemi',
+    author_email='hamed.ghasemi.gl@gmail.com',
+    url='https://github.com/Hamed-Ghasemi/pypersiandate',
+)
