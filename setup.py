1249d3635ac8cefe429bce23d6b19cdbb172b25a
Hamed-Ghasemi <hamed.ghasemi.gl@gmail.com>
2020-12-13 16:17:03 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pypersiandate',
-    version='0.0.3',
+    version='0.0.4',
     description='Python library for converting gregorian date to persian date.',
     author='Hamed Ghasemi',
     author_email='hamed.ghasemi.gl@gmail.com',
